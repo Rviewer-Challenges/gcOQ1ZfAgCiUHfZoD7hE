5507967f13da527e82078f353dfad7f47b53fc86
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2022-05-21 18:44:13 -0600

    PokeApi Service

---
+++ MemoryGame/MemoryGame/MemoryGame/ApiService/PokemonApiService.cs
@@ -0,0 +1,55 @@
+﻿using MemoryGame.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace MemoryGame.ApiService
+{
+    public class PokemonApiService : IPokemonApiService
+    {
+        HttpClient httpClient;
+        public PokemonApiService()
+        {
+            httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(Constants.PokeBaseUrl)
+            };
+        }
+
+        public async Task<PokemonList> GetPokemonList<PokemonList>(int limit)
+        {
+
+            if (Connectivity.NetworkAccess == NetworkAccess.None)
+            {
+                Console.WriteLine("No internet conection");
+                return default; ;
+            }
+
+            try
+            {
+                var url = $"?limit={limit}";
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var pokeObject = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<PokemonList>(pokeObject);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error to get api pokemons list");
+                Console.WriteLine(ex.ToString());
+                return default;
+
+            }
+
+            return default;
+        }
+  
+    }
+}
