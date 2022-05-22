49948b1476c3d719d3954507bd7821fc4a595943
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2022-05-21 18:43:50 -0600

    Add models
    Add models to use in poke api requests

---
+++ MemoryGame/MemoryGame/MemoryGame/Models/PokemonList.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemoryGame.Models
+{
+
+
+
+    public class PokemonList
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("next")]
+        public string Next { get; set; }
+        
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public List<Pokemon> Pokemons { get; set; }
+    }
+
+}
